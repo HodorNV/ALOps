0dd9daa1c595a7eabbb45cd7c0f18a70450922bb
waldo1001 <github@waldo.be>
2020-01-13 15:30:24 +0100

    Install Docker

---
+++ Installation/InstallDockerOnWindowsServer.ps1
@@ -0,0 +1,7 @@
+# If you set up your own agent to run build pipelines, it makes sense to set up Docker.  
+# The below script is the one we always use to install Docker on Windows Server.
+
+Install-WindowsFeature Hyper-V, Containers -Restart
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Confirm:$false -Force
+Install-Module DockerProvider -Confirm:$false -Force
+Install-Package Docker -ProviderName DockerProvider -Confirm:$false -Force
